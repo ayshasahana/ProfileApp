125c9b24ee702aaf6a939e85270f44afc762a3ea
ayshasahana <ayshashana656@gmail.com>
2023-10-28 15:21:51 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+void main()
+{
+ runApp(MaterialApp(home: Center(child: Text("hey flutter",style:TextStyle(fontSize: 80,color: Colors.deepOrange,decoration: TextDecoration.none) ,))));
+}
